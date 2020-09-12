8e1bd65d8b2d81e8e6834b8b892e0d0c1cd17437
jdhughes-usgs <jdhughes@usgs.gov>
2020-09-11 23:12:27 -0400

    ci(rtd): add worklow to create examples rst file from LaTeX document (#30)

--- .doc/test_build.py
+++ .doc/test_build.py
@@ -9,32 +9,32 @@ import pymake
 # -- determine if running on CI or rtd
 is_CI = 'CI' in os.environ or os.environ.get('READTHEDOCS') == 'True'
 
-# -- download executables ----------------------------------------------------
-pth = os.path.join("..", "bin")
-if not os.path.isdir(pth):
-    os.makedirs(pth)
-targets = pymake.usgs_program_data.get_keys(current=True)
-targets.remove("mf6")
-targets.remove("libmf6")
-targets.remove("zbud6")
-pymake.getmfexes(pth, verbose=True, exes=targets)
-
-# -- update mf6 executables with latest nightly build ------------------------
-if is_CI:
-    osname = sys.platform.lower()
-    if osname == "win32":
-        key = "win64.zip"
-    elif osname == "darwin":
-        key = "mac.zip"
-    elif osname == "linux":
-        key = "linux.zip"
-    url = pymake.get_repo_assets("MODFLOW-USGS/modflow6-nightly-build")[key]
-    pymake.download_and_unzip(url, pth, verbose=True)
+# # -- download executables ----------------------------------------------------
+# pth = os.path.join("..", "bin")
+# if not os.path.isdir(pth):
+#     os.makedirs(pth)
+# targets = pymake.usgs_program_data.get_keys(current=True)
+# targets.remove("mf6")
+# targets.remove("libmf6")
+# targets.remove("zbud6")
+# pymake.getmfexes(pth, verbose=True, exes=targets)
+#
+# # -- update mf6 executables with latest nightly build ------------------------
+# if is_CI:
+#     osname = sys.platform.lower()
+#     if osname == "win32":
+#         key = "win64.zip"
+#     elif osname == "darwin":
+#         key = "mac.zip"
+#     elif osname == "linux":
+#         key = "linux.zip"
+#     url = pymake.get_repo_assets("MODFLOW-USGS/modflow6-nightly-build")[key]
+#     pymake.download_and_unzip(url, pth, verbose=True)
 
 # -- update flopy classes ----------------------------------------------------
 flopy.mf6.utils.generate_classes(branch="develop", backup=False)
 
-# -- update notebooks --------------------------------------------------------
+# -- update notebooks and tables ---------------------------------------------
 pth = os.path.join("..", "scripts")
 args = ("python", "process-scripts.py")
 print(" ".join(args))
@@ -59,6 +59,18 @@ if not is_CI:
             print(stdout.decode("utf-8"))
         if stderr:
             print("Errors:\n{}".format(stderr.decode("utf-8")))
+
+# -- create and edit examples.rst and copy the figures -----------------------
+if not is_CI:
+    pth = os.path.join("..", "etc")
+    args = ("python", "ci_create_examples.py")
+    print(" ".join(args))
+    proc = Popen(args, stdout=PIPE, stderr=PIPE, cwd=pth)
+    stdout, stderr = proc.communicate()
+    if stdout:
+        print(stdout.decode("utf-8"))
+    if stderr:
+        print("Errors:\n{}".format(stderr.decode("utf-8")))
 
 # -- get list of notebooks ---------------------------------------------------
 pth = os.path.join("..", "notebooks")
