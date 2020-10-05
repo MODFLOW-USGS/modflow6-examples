8eeb8afcefad4a92a40bf62a6ce2fc14ae8f9bdd
Christian Langevin <langevin@usgs.gov>
2020-10-05 12:48:35 -0500

    add groundwater whirl problem

--- scripts/ex-gwf-whirl.py
+++ scripts/ex-gwf-whirl.py
@@ -1,12 +1,12 @@
-# ## Hani example
+# ## Whirl example
 #
-# Simple steady state model using a regular MODFLOW grid to simulate the
-# response of an anisotropic confined aquifer to a pumping well. A
-# constant-head boundary condition surrounds the active domain.  K22 is set
-# to 0.01.  Drawdown is more pronounced in the K11 direction.
+# This is a 10 layer steady-state problem involving anisotropic groundwater
+# flow.  The XT3D formulation is used to represent variable hydraulic
+# conductivitity ellipsoid orientations.  The resulting flow pattern consists
+# of groundwater whirls, as described in the XT3D documentation report.
 #
 
-# ### Hani Problem Setup
+# ### Whirl Problem Setup
 #
 # Imports
 
@@ -32,6 +32,7 @@ figure_size = (3.5, 3.5)
 
 # Base simulation and model name and workspace
 
+sim_name = "ex-gwf-whirl"
 ws = config.base_ws
 
 # Model units
@@ -41,27 +42,23 @@ time_units = "days"
 
 # Scenario parameters
 
-parameters = {
-    "ex-gwf-hanir": {"angle1": 0, "xt3d": False},
-    "ex-gwf-hanix": {"angle1": 25, "xt3d": True},
-    "ex-gwf-hanic": {"angle1": 90, "xt3d": False},
-}
-
-# Table Hani Model Parameters
+# Table Whirl Model Parameters
 
 nper = 1  # Number of periods
-nlay = 1  # Number of layers
-nrow = 51  # Number of rows
+nlay = 10  # Number of layers
+nrow = 10  # Number of rows
 ncol = 51  # Number of columns
-delr = 10.0  # Spacing along rows ($m$)
-delc = 10.0  # Spacing along columns ($m$)
+delr = 100.0  # Spacing along rows ($m$)
+delc = 100.0  # Spacing along columns ($m$)
 top = 0.0  # Top of the model ($m$)
-botm = -10.0  # Layer bottom elevations ($m$)
+botm_str = "-100, -200, -300, -400, -500, -600, -700, -800, -900, -1000"  # Layer bottom elevations ($m$)
 strt = 0.0  # Starting head ($m$)
 icelltype = 0  # Cell conversion type
-k11 = 1.0  # Horizontal hydraulic conductivity in the 11 direction ($m/d$)
-k22 = 0.01  # Horizontal hydraulic conductivity in the 22 direction ($m/d$)
-pumping_rate = -1.0  # Pumping rate ($m^3/d$)
+k11 = 1.0  # Hydraulic conductivity in the 11 direction ($m/d$)
+k22 = 0.1  # Hydraulic conductivity in the 22 direction ($m/d$)
+k33 = 1.0  # Hydraulic conductivity in the 33 direction ($m/d$)
+angle1_str = "45, 45, 45, 45, 45, -45, -45, -45, -45, -45"  # Rotation of the hydraulic conductivity ellipsoid in the x-y plane
+inflow_rate = 0.01  # Inflow rate ($m^3/d$)
 
 # Static temporal data used by TDIS file
 # Simulation has 1 steady stress period (1 day)
@@ -71,17 +68,22 @@ nstp = [1]
 tsmult = [1.0]
 tdis_ds = list(zip(perlen, nstp, tsmult))
 
+# Parse strings into lists
+
+botm = [float(value) for value in botm_str.split(",")]
+angle1 = [float(value) for value in angle1_str.split(",")]
+
 nouter = 50
 ninner = 100
 hclose = 1e-9
 rclose = 1e-6
 
-# ### Functions to build, write, run, and plot the MODFLOW 6 Hani model
+# ### Functions to build, write, run, and plot the MODFLOW 6 Whirl model
 #
 # MODFLOW 6 flopy simulation object (sim) is returned if building the model
 
 
-def build_model(sim_name, angle1, xt3d):
+def build_model():
     if config.buildModel:
         sim_ws = os.path.join(ws, sim_name)
         sim = flopy.mf6.MFSimulation(
@@ -106,6 +108,8 @@ def build_model(sim_name, angle1, xt3d):
             nlay=nlay,
             nrow=nrow,
             ncol=ncol,
+            delr=delr,
+            delc=delc,
             top=top,
             botm=botm,
         )
@@ -114,21 +118,23 @@ def build_model(sim_name, angle1, xt3d):
             icelltype=icelltype,
             k=k11,
             k22=k22,
+            k33=k33,
             angle1=angle1,
             save_specific_discharge=True,
-            xt3doptions=xt3d,
+            xt3doptions=True,
         )
         flopy.mf6.ModflowGwfic(gwf, strt=strt)
-
-        ibd = -1 * np.ones((nrow, ncol), dtype=np.int)
-        ibd[1:-1, 1:-1] = 1
-        chdrow, chdcol = np.where(ibd == -1)
-        chd_spd = [[0, i, j, 0.0] for i, j in zip(chdrow, chdcol)]
-        flopy.mf6.ModflowGwfchd(
-            gwf, stress_period_data=chd_spd, pname="CHD",
-        )
+        rate = np.zeros((nlay, nrow, ncol), dtype=np.float)
+        rate[:, :, 0] = inflow_rate
+        rate[:, :, -1] = -inflow_rate
+        wellay, welrow, welcol = np.where(rate != 0.0)
+        wel_spd = [
+            ((k, i, j), rate[k, i, j])
+            for k, i, j in zip(wellay, welrow, welcol)
+        ]
+        wel_spd = {0: wel_spd}
         flopy.mf6.ModflowGwfwel(
-            gwf, stress_period_data=[0, 25, 25, pumping_rate], pname="WEL",
+            gwf, stress_period_data=wel_spd, pname="WEL",
         )
         head_filerecord = "{}.hds".format(sim_name)
         budget_filerecord = "{}.cbc".format(sim_name)
@@ -138,12 +144,11 @@ def build_model(sim_name, angle1, xt3d):
             budget_filerecord=budget_filerecord,
             saverecord=[("HEAD", "ALL"), ("BUDGET", "ALL")],
         )
-
         return sim
     return None
 
 
-# Function to write MODFLOW 6 Hani model files
+# Function to write MODFLOW 6 Whirl model files
 
 
 def write_model(sim, silent=True):
@@ -165,70 +170,43 @@ def run_model(sim, silent=False):
     return success
 
 
-# Function to plot the Hani model results.
+# Function to plot the Whirl model results.
 #
-def plot_grid(idx, sim):
+
+
+def plot_spdis(sim):
     fs = USGSFigure(figure_type="map", verbose=False)
-    sim_name = list(parameters.keys())[idx]
     sim_ws = os.path.join(ws, sim_name)
     gwf = sim.get_model(sim_name)
 
     fig = plt.figure(figsize=figure_size)
     fig.tight_layout()
 
-    ax = fig.add_subplot(1, 1, 1, aspect="equal")
-    pmv = flopy.plot.PlotMapView(model=gwf, ax=ax, layer=0)
-    pmv.plot_grid()
-    pmv.plot_bc(name="CHD")
-    pmv.plot_bc(name="WEL")
-    ax.set_xlabel("x position (m)")
-    ax.set_ylabel("y position (m)")
+    # create MODFLOW 6 cell-by-cell budget object
+    file_name = gwf.oc.budget_filerecord.get_data()[0][0]
+    fpth = os.path.join(sim_ws, file_name)
+    cobj = flopy.utils.CellBudgetFile(fpth, precision="double")
+    spdis = cobj.get_data(text="DATA-SPDIS", totim=1.0)
+
+    ax = fig.add_subplot(1, 1, 1)
+    pxs = flopy.plot.PlotCrossSection(model=gwf, ax=ax, line={"column": 0})
+    pxs.plot_grid(linewidth=0.5)
+    pxs.plot_specific_discharge(spdis, normalize=True)
+    ax.set_xlabel("y position (m)")
+    ax.set_ylabel("z position (m)")
 
     # save figure
     if config.plotSave:
         fpth = os.path.join(
-            "..", "figures", "{}-grid{}".format(sim_name, config.figure_ext)
+            "..", "figures", "{}-spdis{}".format(sim_name, config.figure_ext)
         )
         fig.savefig(fpth)
     return
 
 
-def plot_head(idx, sim):
-    fs = USGSFigure(figure_type="map", verbose=False)
-    sim_name = list(parameters.keys())[idx]
-    sim_ws = os.path.join(ws, sim_name)
-    gwf = sim.get_model(sim_name)
-
-    fig = plt.figure(figsize=figure_size)
-    fig.tight_layout()
-
-    fname = os.path.join(sim_ws, "{}.hds".format(sim_name))
-    hdobj = flopy.utils.HeadFile(fname)
-    head = hdobj.get_data()
-
-    ax = fig.add_subplot(1, 1, 1, aspect="equal")
-    pmv = flopy.plot.PlotMapView(model=gwf, ax=ax, layer=0)
-    cb = pmv.plot_array(0 - head, cmap="jet", alpha=0.25)
-    cs = pmv.contour_array(0 - head, levels=np.arange(0.1, 1, 0.1))
-    cbar = plt.colorbar(cb, shrink=0.25)
-    cbar.ax.set_xlabel(r"Drawdown, ($m$)")
-    ax.set_xlabel("x position (m)")
-    ax.set_ylabel("y position (m)")
-
-    # save figure
-    if config.plotSave:
-        fpth = os.path.join(
-            "..", "figures", "{}-head{}".format(sim_name, config.figure_ext)
-        )
-        fig.savefig(fpth)
-    return
-
-
-def plot_results(idx, sim, silent=True):
+def plot_results(sim, silent=True):
     if config.plotModel:
-        if idx == 0:
-            plot_grid(idx, sim)
-        plot_head(idx, sim)
+        plot_spdis(sim)
     return
 
 
@@ -242,13 +220,11 @@ def plot_results(idx, sim, silent=True):
 
 
 def simulation(idx, silent=True):
-    key = list(parameters.keys())[idx]
-    params = parameters[key].copy()
-    sim = build_model(key, **params)
+    sim = build_model()
     write_model(sim, silent=silent)
     success = run_model(sim, silent=silent)
     if success:
-        plot_results(idx, sim, silent=silent)
+        plot_results(sim, silent=silent)
 
 
 # nosetest - exclude block from this nosetest to the next nosetest
@@ -256,27 +232,11 @@ def test_01():
     simulation(0, silent=False)
 
 
-def test_02():
-    simulation(1, silent=False)
-
-
-def test_03():
-    simulation(2, silent=False)
-
-
 # nosetest end
 
 if __name__ == "__main__":
-    # ### Hani Simulation
+    # ### Whirl Simulation
     #
-    # Simulated heads in the Hani model with anisotropy in x direction.
+    # Simulated heads in the Whirl model with anisotropy in x direction.
 
     simulation(0)
-
-    # Simulated heads in the Hani model with anisotropy in y direction.
-
-    simulation(1)
-
-    # Simulated heads in the Hani model with anisotropy rotated 15 degrees.
-
-    simulation(2)
