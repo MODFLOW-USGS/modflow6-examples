e2e0094afbaf8bd8e8799243babaa1c5bb010840
jdhughes-usgs <jdhughes@usgs.gov>
2020-10-12 17:01:09 -0400

    doc(lak): Add Lake Package Problem 1 and 2

--- scripts/ex-gwf-lak-p02.py
+++ scripts/ex-gwf-lak-p02.py
@@ -1,10 +1,10 @@
-# ## Lake package (LAK) Package problem 1
+# ## Lake package (LAK) Package problem 2
 #
-# This is the lake package example problem (test 1) from the
+# This is the lake package example problem (test 2) from the
 # Lake Package documentation (Merritt and Konikow, 2000).
 #
 
-# ### LAK Package Problem 1 Setup
+# ### LAK Package problem 2 Setup
 #
 # Imports
 
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import flopy
+import shapefile as shp
 
 # Append to system path to include the common subdirectory
 
@@ -35,24 +36,24 @@ ws = config.base_ws
 
 # Simulation name
 
-sim_name = "ex-gwf-lak-p01"
+sim_name = "ex-gwf-lak-p02"
 
 # Model units
 
 length_units = "feet"
 time_units = "days"
 
-# Table LAK Package Problem 1 Model Parameters
+# Table LAK Package problem 2 Model Parameters
 
 nper = 1  # Number of periods
 nlay = 5  # Number of layers
-nrow = 17  # Number of rows
+nrow = 27  # Number of rows
 ncol = 17  # Number of columns
-top = 500.0  # Top of the model ($ft$)
-botm_str = "107., 97., 87., 77., 67."  # Bottom elevations ($ft$)
+top = 200.0  # Top of the model ($ft$)
+botm_str = "102., 97., 87., 77., 67."  # Bottom elevations ($ft$)
 strt = 115.0  # Starting head ($ft$)
 k11 = 30.0  # Horizontal hydraulic conductivity ($ft/d$)
-k33_str = "1179., 30., 30., 30., 30."  # Vertical hydraulic conductivity ($ft/d$)
+k33 = 30.0  # Vertical hydraulic conductivity ($ft/d$)
 ss = 3e-4  # Specific storage ($1/d$)
 sy = 0.2  # Specific yield (unitless)
 H1 = 160.0  # Constant head on left side of model ($ft$)
@@ -60,17 +61,16 @@ H2 = 140.0  # Constant head on right side of model ($ft$)
 recharge = 0.0116  # Aereal recharge rate ($ft/d$)
 etvrate = 0.0141  # Maximum evapotranspiration rate ($ft/d$)
 etvdepth = 15.0  # Evapotranspiration extinction depth ($ft$)
-lak_strt = 110.0  # Starting lake stage ($ft$)
+lak_strt = 130.0  # Starting lake stage ($ft$)
 lak_etrate = 0.0103  # Lake evaporation rate ($ft/d$)
 
 # parse parameter strings into tuples
 
 botm = [float(value) for value in botm_str.split(",")]
-k33 = [float(value) for value in k33_str.split(",")]
 
 # Static temporal data used by TDIS file
 
-tdis_ds = ((5000.0, 100, 1.02),)
+tdis_ds = ((1500.0, 200, 1.005),)
 
 # define delr and delc
 delr = np.array(
@@ -81,9 +81,9 @@ delr = np.array(
         1000.0,
         1000.0,
         1000.0,
-        500.00,
-        500.00,
-        500.00,
+        500.0,
+        500.0,
+        500.0,
         500.0,
         500.00,
         1000.0,
@@ -102,11 +102,21 @@ delc = np.array(
         1000.0,
         1000.0,
         1000.0,
-        500.00,
-        500.00,
-        500.00,
         500.0,
-        500.00,
+        500.0,
+        500.0,
+        500.0,
+        500.0,
+        1000.0,
+        1000.0,
+        1000.0,
+        1000.0,
+        1000.0,
+        500.0,
+        500.0,
+        500.0,
+        500.0,
+        500.0,
         1000.0,
         1000.0,
         1000.0,
@@ -143,7 +153,7 @@ surf = np.tile(s1d, (nrow, 1))
 surf[idomain0 == 0] = botm[0] - 2
 surf[idomain1 == 0] = botm[1] - 2
 
-# ### Create LAK Package Problem 1 Model Boundary Conditions
+# ### Create LAK Package problem 2 Model Boundary Conditions
 #
 # Constant head boundary conditions
 #
@@ -155,7 +165,10 @@ for k in range(nlay):
 
 # LAK Package
 
-lak_conn = [
+lak_time_conv = 86400.
+lak_len_conv = 3.28081
+
+lak0_conn = [
     [0, 0, 0, 6, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
     [0, 1, 0, 7, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
     [0, 2, 0, 8, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
@@ -215,11 +228,154 @@ lak_conn = [
     [0, 56, 0, 10, 11, "HORIZONTAL", 0.1, 0, 0, 500, 500],
 ]
 
-lak_packagedata = [[0, lak_strt, len(lak_conn)]]
+lak1_conn = [
+    [1, 0, 0, 16, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 1, 0, 17, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 2, 0, 18, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 3, 0, 19, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 4, 0, 20, 5, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 5, 0, 15, 6, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 6, 1, 16, 6, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 7, 1, 17, 6, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 8, 1, 17, 6, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 9, 1, 18, 6, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 10, 1, 18, 6, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 11, 1, 19, 6, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 12, 1, 19, 6, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 13, 1, 20, 6, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 14, 0, 21, 6, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 15, 0, 15, 7, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 16, 1, 16, 7, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 17, 1, 16, 7, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 18, 2, 17, 7, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 19, 2, 18, 7, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 20, 2, 19, 7, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 21, 1, 20, 7, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 22, 1, 20, 7, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 23, 0, 21, 7, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 24, 0, 15, 8, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 25, 1, 16, 8, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 26, 1, 16, 8, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 27, 2, 17, 8, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 28, 2, 18, 8, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 29, 2, 19, 8, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 30, 1, 20, 8, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 31, 1, 20, 8, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 32, 0, 21, 8, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 33, 0, 15, 9, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 34, 1, 16, 9, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 35, 1, 16, 9, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 36, 2, 17, 9, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 37, 2, 18, 9, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 38, 2, 19, 9, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 39, 1, 20, 9, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 40, 1, 20, 9, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 41, 0, 21, 9, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 42, 0, 15, 10, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 43, 1, 16, 10, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 44, 1, 17, 10, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 45, 1, 17, 10, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 46, 1, 18, 10, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 47, 1, 18, 10, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 48, 1, 19, 10, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 49, 1, 19, 10, "HORIZONTAL", 0.1, 0, 0, 250, 500],
+    [1, 50, 1, 20, 10, "VERTICAL", 0.1, 0, 0, 0, 0],
+    [1, 51, 0, 21, 10, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 52, 0, 16, 11, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 53, 0, 17, 11, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 54, 0, 18, 11, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 55, 0, 19, 11, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+    [1, 56, 0, 20, 11, "HORIZONTAL", 0.1, 0, 0, 500, 500],
+]
+
+lak_packagedata = [
+    [0, lak_strt, len(lak0_conn)],
+    [1, lak_strt, len(lak1_conn)],
+]
+
+lak_outlets = [
+    [0, 0, -1, "manning", 114.85, 5., 0.05, 8.206324419006205e-4],
+    [1, 1, -1, "manning", 109.4286, 5., 0.05, 9.458197164349258e-4],
+]
 
 lak_spd = [
     [0, "rainfall", recharge],
     [0, "evaporation", lak_etrate],
+    [1, "rainfall", recharge],
+    [1, "evaporation", lak_etrate],
+]
+
+# SFR package
+
+sfr_conv = 128390.4
+
+sfr_pakdata = [
+    [0, 0, 1, 4, 1000, 5, 0.001103448, 123.94827, 0.5, 0.5, 0.050000001, 1, 1, 0],
+    [1, 0, 2, 4, 1000, 5, 0.001103448, 122.84483, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [2, 0, 3, 4, 1000, 5, 0.001103448, 121.74138, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [3, 0, 3, 5, 1000, 5, 0.001103448, 120.63793, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [4, 0, 3, 6, 500, 5, 0.001103448, 119.81035, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [5, 0, 3, 7, 750, 5, 0.001103448, 119.12069, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [6, 0, 4, 7, 1000, 5, 0.001103448, 118.15517, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [7, 0, 5, 7, 1000, 5, 0.001103448, 117.05173, 0.5, 0.5, 0.050000001, 1, 1, 0],
+    [8, 0, 11, 8, 1000, 5, 0.000820632, 114.43968, 0.5, 0.5, 0.050000001, 1, 1, 0],
+    [9, 0, 12, 8, 1000, 5, 0.000820632, 113.61905, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [10, 0, 13, 9, 559, 5, 0.000820632, 112.97937, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [11, 0, 13, 9, 559, 5, 0.000820632, 112.52063, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [12, 0, 14, 9, 1000, 5, 0.000820632, 111.88095, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [13, 0, 15, 9, 1000, 5, 0.000820632, 111.06032, 0.5, 0.5, 0.050000001, 1, 1, 0],
+    [14, 0, 21, 9, 1000, 5, 0.00094582, 108.95569, 0.5, 0.5, 0.050000001, 1, 1, 0],
+    [15, 0, 22, 9, 750, 5, 0.00094582, 108.1281, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [16, 0, 22, 10, 500, 5, 0.00094582, 107.53696, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [17, 0, 22, 11, 1000, 5, 0.00094582, 106.82759, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [18, 0, 22, 12, 1000, 5, 0.00094582, 105.88177, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [19, 0, 22, 13, 1000, 5, 0.00094582, 104.93595, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [20, 0, 22, 14, 1000, 5, 0.00094582, 103.99014, 0.5, 0.5, 0.050000001, 2, 1, 0],
+    [21, 0, 22, 15, 1000, 5, 0.00094582, 103.04431, 0.5, 0.5, 0.050000001, 1, 1, 0],
+]
+
+sfr_conn = [
+    [0, -1],
+    [1, 0, -2],
+    [2, 1, -3],
+    [3, 2, -4],
+    [4, 3, -5],
+    [5, 4, -6],
+    [6, 5, -7],
+    [7, 6],
+    [8, -9],
+    [9, 8, -10],
+    [10, 9, -11],
+    [11, 10, -12],
+    [12, 11, -13],
+    [13, 12],
+    [14, -15],
+    [15, 14, -16],
+    [16, 15, -17],
+    [17, 16, -18],
+    [18, 17, -19],
+    [19, 18, -20],
+    [20, 19, -21],
+    [21, 20],
+]
+
+sfr_spd = [
+    [0, "inflow", 691200.0]
+]
+
+# MVR package
+
+mvr_paks = [
+    ["SFR-1"],
+    ["LAK-1"],
+]
+
+mvr_spd = [
+    ["SFR-1", 7, "LAK-1", 0, "FACTOR", 1.0],
+    ["LAK-1", 0, "SFR-1", 8, "FACTOR", 1.0],
+    ["SFR-1", 13, "LAK-1", 1, "FACTOR", 1.0],
+    ["LAK-1",  1,"SFR-1", 14, "FACTOR", 0.5],
+
 ]
 
 # Solver parameters
@@ -230,7 +386,7 @@ hclose = 1e-9
 rclose = 1e-6
 
 
-# ### Functions to build, write, run, and plot the MODFLOW 6 LAK Package Problem 1 model
+# ### Functions to build, write, run, and plot the MODFLOW 6 LAK Package problem 2 model
 #
 # MODFLOW 6 flopy simulation object (sim) is returned if building the model
 
@@ -301,21 +457,44 @@ def build_model():
         )
         lak = flopy.mf6.ModflowGwflak(
             gwf,
-            nlakes=1,
-            noutlets=0,
+            pname="LAK-1",
+            time_conversion=lak_time_conv,
+            length_conversion=lak_len_conv,
+            mover=True,
+            nlakes=2,
+            noutlets=len(lak_outlets),
             packagedata=lak_packagedata,
-            connectiondata=lak_conn,
+            connectiondata=lak0_conn + lak1_conn,
+            outlets=lak_outlets,
             perioddata=lak_spd,
         )
         obs_file = "{}.lak.obs".format(sim_name)
         csv_file = obs_file + ".csv"
         obs_dict = {
             csv_file: [
-                ("stage", "stage", (0,)),
+                ("lake1", "stage", (0,)),
+                ("lake2", "stage", (1,)),
             ]
         }
         lak.obs.initialize(
             filename=obs_file, digits=10, print_input=True, continuous=obs_dict
+        )
+        flopy.mf6.ModflowGwfsfr(
+            gwf,
+            pname="SFR-1",
+            unit_conversion=sfr_conv,
+            mover=True,
+            nreaches=len(sfr_pakdata),
+            packagedata=sfr_pakdata,
+            connectiondata=sfr_conn,
+            perioddata=sfr_spd,
+        )
+        flopy.mf6.ModflowGwfmvr(
+            gwf,
+            maxmvr=4,
+            maxpackages=2,
+            packages=mvr_paks,
+            perioddata=mvr_spd,
         )
 
         head_filerecord = "{}.hds".format(sim_name)
@@ -330,7 +509,7 @@ def build_model():
     return None
 
 
-# Function to write MODFLOW 6 LAK Package Problem 1 model files
+# Function to write MODFLOW 6 LAK Package problem 2 model files
 
 
 def write_model(sim, silent=True):
@@ -338,7 +517,7 @@ def write_model(sim, silent=True):
         sim.write_simulation(silent=silent)
 
 
-# Function to run the LAK Package Problem 1 model.
+# Function to run the LAK Package problem 2 model.
 # True is returned if the model runs successfully
 #
 
@@ -357,6 +536,52 @@ def run_model(sim, silent=True):
 
 def plot_grid(gwf, silent=True):
     sim_ws = os.path.join(ws, sim_name)
+
+    # create lake array
+    ilake = gwf.dis.idomain.array
+    ilake[ilake == 0] = 100
+    ilake[ilake == 1] = 0
+    ilake[ilake == 100] = 1
+    for k in range(nlay):
+        for i in range(16, nrow):
+            for j in range(ncol):
+                if ilake[k, i, j] == 1:
+                    ilake[k, i, j] = 2
+
+    # get edges and centers of cells
+    xedges, yedges = gwf.modelgrid.xyedges[0], gwf.modelgrid.xyedges[1]
+    xcenters, ycenters = gwf.modelgrid.xycenters[0], gwf.modelgrid.xycenters[1]
+
+    # create sfr network
+    poly0 = [
+        [xcenters[4], yedges[1]],
+        [xcenters[4], ycenters[4]],
+        [xcenters[7], ycenters[4]],
+        [xcenters[7], yedges[6]],
+    ]
+    poly1 = [
+        [xcenters[8], yedges[11]],
+        [xcenters[8], yedges[13]],
+        [xcenters[9], yedges[14]],
+        [xcenters[9], yedges[16]],
+    ]
+    poly2 = [
+        [xcenters[9], yedges[21]],
+        [xcenters[9], ycenters[22]],
+        [xedges[16], ycenters[22]],
+    ]
+    parts = [poly0, poly1, poly2]
+    shape_pth = os.path.join(ws, sim_name, "sfr.shp")
+    w = shp.Writer(target=shape_pth, shapeType=shp.POLYLINE)
+    w.field("no", "C")
+    w.line([poly0])
+    w.record(["1"])
+    w.line([poly1])
+    w.record(["2"])
+    w.line([poly2])
+    w.record(["3"])
+    w.close()
+    sfr = shp.Reader(shape_pth)
 
     # load the observations
     fpth = os.path.join(ws, sim_name, "{}.lak.obs.csv".format(sim_name))
@@ -378,10 +603,10 @@ def plot_grid(gwf, silent=True):
     spdis = cobj.get_data(text="DATA-SPDIS", kstpkper=kstpkper[0])
 
     # add lake stage to heads
-    head[head == 1e30] = lak_results["STAGE"][-1]
+    head[ilake == 1] = lak_results["LAKE1"][-1]
+    head[ilake == 2] = lak_results["LAKE2"][-1]
 
     # observation locations
-    xcenters, ycenters = gwf.modelgrid.xycenters[0], gwf.modelgrid.xycenters[1]
     p1 = (xcenters[3], ycenters[3])
     p2 = (xcenters[13], ycenters[13])
 
@@ -393,14 +618,12 @@ def plot_grid(gwf, silent=True):
     plt.axis("off")
 
     nrows, ncols = 10, 1
-    axes = [fig.add_subplot(nrows, ncols, (1, 5))]
-    axes.append(fig.add_subplot(nrows, ncols, (6, 8), sharex=axes[0]))
+    axes = [fig.add_subplot(nrows, ncols, (1, 8))]
 
     for idx, ax in enumerate(axes):
         ax.set_xlim(extents[:2])
-        if idx == 0:
-            ax.set_ylim(extents[2:])
-            ax.set_aspect("equal")
+        ax.set_ylim(extents[2:])
+        ax.set_aspect("equal")
 
     # legend axis
     axes.append(fig.add_subplot(nrows, ncols, (9, 10)))
@@ -423,11 +646,15 @@ def plot_grid(gwf, silent=True):
     ax = axes[0]
     mm = flopy.plot.PlotMapView(gwf, ax=ax, extent=extents)
     mm.plot_bc("CHD", color="cyan")
+    for shape in sfr.shapeRecords():
+        x = [i[0] for i in shape.shape.points[:]]
+        y = [i[1] for i in shape.shape.points[:]]
+        ax.plot(x, y, color="#3BB3D0", lw=1.5)
     mm.plot_inactive(color_noflow="#5DBB63")
     mm.plot_grid(lw=0.5, color="black")
     cv = mm.contour_array(
         head,
-        levels=np.arange(140, 160, 2),
+        levels=np.arange(120, 160, 5),
         linewidths=0.75,
         linestyles="-",
         colors="blue",
@@ -464,18 +691,6 @@ def plot_grid(gwf, silent=True):
     )
     fs.remove_edge_ticks(ax)
 
-    ax = axes[1]
-    xs = flopy.plot.PlotCrossSection(gwf, ax=ax, line={"row": 8})
-    xs.plot_array(np.ones(shape3d), head=head, cmap="jet")
-    xs.plot_bc("CHD", color="cyan", head=head)
-    xs.plot_ibound(color_noflow="#5DBB63", head=head)
-    xs.plot_grid(lw=0.5, color="black")
-    ax.set_xlabel("x-coordinate, in feet")
-    ax.set_ylim(67, 160)
-    ax.set_ylabel("Elevation, in feet")
-    fs.heading(ax, heading="Cross-section view", idx=1)
-    fs.remove_edge_ticks(ax)
-
     # legend
     ax = axes[-1]
     ax.plot(
@@ -503,13 +718,9 @@ def plot_grid(gwf, silent=True):
     ax.plot(
         -10000,
         -10000,
-        lw=0,
-        marker="s",
-        ms=10,
-        mfc="blue",
-        mec="black",
-        markeredgewidth=0.5,
-        label="Water table",
+        lw=1.5,
+        color="#3BB3D0",
+        label="Stream network",
     )
     ax.plot(
         -10000,
@@ -568,39 +779,59 @@ def plot_lak_results(gwf, silent=True):
 
     dtype = [
         ("time", np.float),
-        ("STAGE", np.float),
+        ("LAKE1", np.float),
+        ("LAKE2", np.float),
         ("A", np.float),
         ("B", np.float),
     ]
 
     results = np.zeros((lak_results.shape[0] + 1), dtype=dtype)
     results["time"][1:] = lak_results["time"]
-    results["STAGE"][0] = 110.0
-    results["STAGE"][1:] = lak_results["STAGE"]
-    results["A"][0] = 115.0
+    results["LAKE1"][0] = lak_strt
+    results["LAKE1"][1:] = lak_results["LAKE1"]
+    results["LAKE2"][0] = lak_strt
+    results["LAKE2"][1:] = lak_results["LAKE2"]
+    results["A"][0] = strt
     results["A"][1:] = gwf_results["A"]
-    results["B"][0] = 115.0
+    results["B"][0] = strt
     results["B"][1:] = gwf_results["B"]
 
     # create the figure
-    fig, ax = plt.subplots(
+    fig, axes = plt.subplots(
         ncols=1,
-        nrows=1,
+        nrows=2,
         sharex=True,
-        figsize=(6.3, 3.15),
+        figsize=(6.3, 6.3),
         constrained_layout=True,
     )
 
-    ax.set_xlim(0, 3000)
-    ax.set_ylim(110, 160)
+    ax = axes[0]
+    ax.set_xlim(0, 1500)
+    ax.set_ylim(110, 130)
     ax.plot(
         results["time"],
-        results["STAGE"],
+        results["LAKE1"],
         lw=0.75,
         ls="--",
         color="black",
-        label="Lake stage",
+        label="Lake 1 stage",
     )
+    ax.plot(
+        results["time"],
+        results["LAKE2"],
+        lw=0.75,
+        ls="-.",
+        color="black",
+        label="Lake 2 stage",
+    )
+    ax.set_yticks([110, 115, 120, 125, 130])
+    ax.set_ylabel("Lake stage, in feet")
+    fs.graph_legend(ax, loc="upper right")
+
+
+    ax = axes[1]
+    ax.set_xlim(0, 1500)
+    ax.set_ylim(110, 160)
     ax.plot(
         results["time"],
         results["A"],
@@ -618,7 +849,8 @@ def plot_lak_results(gwf, silent=True):
         label="Point B",
     )
     ax.set_xlabel("Simulation time, in days")
-    ax.set_ylabel("Head or stage, in feet")
+    ax.set_yticks([110, 120, 130, 140, 150, 160])
+    ax.set_ylabel("Head, in feet")
     fs.graph_legend(ax, loc="lower right")
 
     # save figure
@@ -633,7 +865,7 @@ def plot_lak_results(gwf, silent=True):
     return
 
 
-# Function to plot the LAK Package Problem 1 model results.
+# Function to plot the LAK Package problem 2 model results.
 
 
 def plot_results(sim, silent=True):
@@ -645,7 +877,7 @@ def plot_results(sim, silent=True):
         plot_lak_results(gwf, silent=silent)
 
 
-# Function that wraps all of the steps for the LAK Package Problem 1 model
+# Function that wraps all of the steps for the LAK Package problem 2 model
 #
 # 1. build_model,
 # 2. write_model,
@@ -674,7 +906,7 @@ def test_01():
 # nosetest end
 
 if __name__ == "__main__":
-    # ### LAK Package Problem 1 Simulation
+    # ### LAK Package problem 2 Simulation
     #
 
     simulation()
