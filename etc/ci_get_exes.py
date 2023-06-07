8b480fb51700cd3d31301abbc5748195b4f1cddd
Joseph Hughes <jdhughes@usgs.gov>
2023-06-07 15:54:23 -0500

    example(synthetic-valley): Add Synthetic Valley GWF and GWT example

--- etc/ci_get_exes.py
+++ etc/ci_get_exes.py
@@ -2,29 +2,29 @@ import os
 import sys
 import numpy as np
 import matplotlib as mpl
+import pathlib as pl
 import flopy
-import pymake
 
 # Print python package versions
 flopypth = flopy.__path__[0]
-pymakepth = pymake.__path__[0]
 print("python version:     {}".format(sys.version))
 print("numpy version:      {}".format(np.__version__))
 print("matplotlib version: {}".format(mpl.__version__))
 print("flopy version:      {}".format(flopy.__version__))
-print("pymake version:     {}".format(pymake.__version__))
 print("")
 print("flopy is installed in:  {}".format(flopypth))
-print("pymake is installed in: {}".format(pymakepth))
 
 # Update flopy from GitHub repo
 flopy.mf6.utils.generate_classes(branch="develop", backup=False)
 
+# Set the directory for the binary files and make the
+# directory if it does not exist
+bin_pth = pl.Path("../bin")
+bin_pth.mkdir(parents=True, exist_ok=True)
+
 # Get executables
-bin_pth = os.path.join("..", "bin")
-if not os.path.isdir(bin_pth):
-    os.makedirs(bin_pth)
-pymake.getmfexes(pth=bin_pth, verbose=True)
+# get the latest executable release
+os.system(f"get-modflow {bin_pth}")
 
 # Replace MODFLOW 6 executables with the latest versions
-pymake.getmfnightly(pth=bin_pth, exes=["mf6", "libmf6"], verbose=True)
+os.system(f"get-modflow --repo modflow6-nightly-build {bin_pth}")
