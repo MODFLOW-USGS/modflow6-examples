ae5fc1599a4c0aeacc1c474ec35fbbf5bfcc59c7
langevin-usgs <langevin@usgs.gov>
2024-05-21 06:54:44 -0500

    fix(ex-gwe-prt): use relative path in fmi in gwe-prt.py (#198)

--- scripts/ex-gwe-prt.py
+++ scripts/ex-gwe-prt.py
@@ -332,8 +332,16 @@ def build_gwe_sim(name):
     )
 
     pd = [
-        ("GWFHEAD", gwf_ws / f"{gwf_name}.hds", None),
-        ("GWFBUDGET", gwf_ws / f"{gwf_name}.cbc", None),
+        (
+            "GWFHEAD",
+            pl.Path(f"../{gwf_ws.name}/{gwf_name}.hds"),
+            None,
+        ),
+        (
+            "GWFBUDGET",
+            pl.Path(f"../{gwf_ws.name}/{gwf_name}.cbc"),
+            None,
+        ),
     ]
     flopy.mf6.ModflowGwefmi(gwe, packagedata=pd)
 
